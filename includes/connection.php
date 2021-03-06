1decb957b531e2d776acb0f28872b1f4f466c3cc
henneall <hentanan53@gmail.com>
2021-03-06 10:42:53 +0800

    update

--- includes/connection.php
+++ includes/connection.php
@@ -1,5 +1,5 @@
 <?php
-	$con=mysqli_connect("localhost","root","1t@dm1N","db_filing");
+	$con=mysqli_connect("localhost","root","","db_filing");
 	if (mysqli_connect_errno()){
 		echo "Failed to connect to MySQL:".mysqli_connect_error();
 	}
